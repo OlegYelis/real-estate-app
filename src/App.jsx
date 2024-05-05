4bdb13ea4533dbaab77d70d08eb181a6e198421c
Oleg Yelisieiev <dodgeoleg@gmail.com>
2024-05-05 16:38:26 +0300

    created header and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
 import "./App.css";
+import { Button } from "./components/Button/Button";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Link to="/">Home</Link>
+      <Link to="/profile">Profile</Link>
+      <Button>Click me</Button>
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { HomePage } from "./pages/HomePage/HomePage";
+import { ProfilePage } from "./pages/ProfilePage/ProfilePage";
+import { SearchPage } from "./pages/SearchPage/SearchPage";
+import { Layout } from "./components/Layout/Layout";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      { path: "/property", element: <SearchPage /> },
+      { path: "/profile", element: <ProfilePage /> },
+    ],
+  },
+  { path: "*", element: <SearchPage /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
