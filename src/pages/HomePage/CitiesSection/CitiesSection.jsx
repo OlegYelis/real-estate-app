bd502bac871be1a7137d1f58cc68e95c065c86d6
Oleg Yelisieiev <dodgeoleg@gmail.com>
2024-05-10 17:01:16 +0300

    added cities section

---
+++ src/pages/HomePage/CitiesSection/CitiesSection.jsx
@@ -0,0 +1,81 @@
+import { Link } from "react-router-dom";
+import styles from "./CitiesSection.module.css";
+
+export const CitiesSection = () => {
+  return (
+    <section className={styles.cities}>
+      <div className={styles.container}>
+        <div className={styles.cities__header}>
+          <h2 className={styles.cities__title}>
+            Нерухомість у Провідних Містах
+          </h2>
+          <p className={styles.cities__descr}>
+            Відкрийте для себе виняткові пропозиції нерухомості у великих містах
+            з чудовими локаціями та першокласними умовами.
+          </p>
+        </div>
+
+        <div className={styles.cities__wrap}>
+          <Link
+            to="/property"
+            className={`${styles.cities__link} ${styles.cities__kyiv}`}
+          >
+            Київ
+            <span>
+              столиця України, відома своїми історичними пам&#39;ятками,
+              розвиненою інфраструктурою та культурним розмаїттям.
+            </span>
+          </Link>
+          <div className={styles.cities_row}>
+            <Link
+              to="/property"
+              className={`${styles.cities__link} ${styles.cities__lviv}`}
+            >
+              Львів
+              <span>
+                місто з багатою історією та культурою, відоме своєю архітектурою
+                та атмосферними кав&#39;ярнями.
+              </span>
+            </Link>
+            <Link
+              to="/property"
+              className={`${styles.cities__link} ${styles.cities__odesa}`}
+            >
+              Одеса
+              <span>
+                портове місто на березі Чорного моря, знамените своїм гумором,
+                пляжами та культурним розмаїттям.
+              </span>
+            </Link>
+          </div>
+          <div className={styles.cities_row}>
+            <Link
+              to="/property"
+              className={`${styles.cities__link} ${styles.cities__kharkiv}`}
+            >
+              Харків
+              <span>
+                друге за величиною місто в країні, важливий освітній та науковий
+                центр.
+              </span>
+            </Link>
+            <Link
+              to="/property"
+              className={`${styles.cities__link} ${styles.cities__dnipro}`}
+            >
+              Дніпро
+              <span>
+                великий промисловий та бізнес-центр, відомий своєю сучасною
+                інфраструктурою та природою навколо.
+              </span>
+            </Link>
+          </div>
+        </div>
+
+        <Link to="/property" className={styles.cities__btn}>
+          Дивитись більше
+        </Link>
+      </div>
+    </section>
+  );
+};
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,15 @@
 import { HeroSection } from "./HeroSection/HeroSection";
 import { FeaturedSection } from "./FeaturedSection/FeaturedSection";
 import { TypesSection } from "./TypesSection/TypesSection";
+import { CitiesSection } from "./CitiesSection/CitiesSection";
 
 export const HomePage = () => {
   return (
     <>
       <HeroSection />
-
       <FeaturedSection />
-
       <TypesSection />
+      <CitiesSection />
     </>
   );
 };
